be7f218b745ee9b768fa06a27665c49d800a4566
Henrique9576 <henrique_costa02@outlook.com>
2023-10-22 17:34:24 -0300

    Initial commit

---
+++ app/src/main/java/Aula4RClasses/Pessoa.java
@@ -0,0 +1,34 @@
+package Aula4RClasses;
+
+public class Pessoa {
+    
+    //Atributos
+    private float peso;
+    private float altura;
+    
+    public Pessoa(float peso, float altura){
+        this.peso = peso;
+        this.altura = altura;
+    }
+    
+    //Métodos
+    public float calcularIMC(){
+        float imc = peso / (altura * altura);
+        return imc;
+    }
+    
+    //Métodos acessores
+    public void setPeso(float peso){
+        this.peso = peso;
+    }
+    public float getPeso(){
+        return peso;
+    }
+    
+    public void setAltura(float altura){
+        this.altura = altura;
+    }
+    public float getAltura(){
+        return altura;
+    }
+}
